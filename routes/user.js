d7f7f5a574eeeda815a10c57c3c2e9aa04098eb1
anurag saran <anuragsaran@unused-10-15-21-55.yyz.redhat.com>
2016-04-18 14:26:59 -0400

    Fixed Signup Error

--- routes/user.js
+++ routes/user.js
@@ -1,31 +1,37 @@
 var router = require('express').Router();
 var User = require('../models/user');
 
-router.get('/signup', function(req,res, next) {
-  res.render('accounts/signup', {errors: req.flash('errors')
-   });  
+router.get('/signup', function(req, res, next) {
+  res.render('accounts/signup', {
+    errors: req.flash('errors')
+  });
 });
 
-router.post('/signup', function(req,res, next) {
-  
-    var user = new User();
-    user.profile.name = req.body.name;
-    user.password = req.body.password;
-    user.email = req.body.email;
-   
-    User.findOne({email: req.body.email}, function(err,existingUser){
-        if(existingUser) {
-            console.log(req.body.email + " is already exist");
-            req.flash('errrors', 'Account with this email address already exists');
-            return res.redirect('/signup');
-        } else {
-            user.save(function(err) {
-            if(err) return next(err);
-                return res.redirect('/');
-            //res.json('Successfully created new user');
-          })
-        }
-    });
+router.post('/signup', function(req, res, next) {
+  var user = new User();
+
+  user.profile.name = req.body.name;
+  user.email = req.body.email;
+  user.password = req.body.password;
+  //user.profile.picture = user.gravatar();
+
+  User.findOne({ email: req.body.email }, function(err, existingUser) {
+
+    if (existingUser) {
+      req.flash('errors', 'Account with that email address already exists');
+      return res.redirect('/signup');
+    } else {
+      user.save(function(err, user) {
+        if (err) return next(err);
+
+        req.logIn(user, function(err) {
+          if (err) return next(err);
+          res.redirect('/profile');
+
+        })
+      });
+    }
+  });
 });
 
 module.exports = router;
